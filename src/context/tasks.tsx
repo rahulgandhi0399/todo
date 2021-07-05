b2cd172e17fe6c8b55da7e089c05cdeffd17ac77
rahulgandhi0399 <60395348+rahulgandhi0399@users.noreply.github.com>
2021-07-05 19:04:56 +0530

    newcontext added

--- src/context/tasks.tsx
+++ src/context/tasks.tsx
@@ -1,67 +1,73 @@
 import * as React from "react";
+import { createContext, useContext } from 'react';
 
-export const TodoContext = React.createContext({});
 
+export type ThemeContextType = {
+  
+  setTasks : (tasksContext: any  ) => void;
+  getTasks : (tasksContext: any  ) => void;
+  setSelectedTask : (tasksContext: any  ) => void;
+  getSelectedTask : (tasksContext: any  ) => void; 
+ 
 
-const TodoProvider: React.FC<React.ReactNode> = ({ children }) => {
-  const [todos, setTodos] = React.useState<any>({
+}
+const setTasks = (tasksContext: any = []) => {
+  const newTodo = {
     id: Math.random(), 
-    title: "abc",
-    description: "xyz",
+    title: tasksContext.title,
+    description: tasksContext.description,
     status: false
-  });
+  }
+   const tasksValue = JSON.stringify(tasksContext);
 
-  
-   const setTasks = (tasksContext: any = []) => {
-    const newTodo = {
-      id: Math.random(), 
-      title: tasksContext.title,
-      description: tasksContext.description,
-      status: false
-    };
-    setTodos([...todos, newTodo]);
-    const tasksValue = JSON.stringify(tasksContext);
-    
-    localStorage.setItem("tasks", tasksValue);
-  };
-  
-   const getTasks = () => {
-    let tasks = localStorage.getItem("tasks");
-    if (!tasks) {
-      return [];
-    }
-    return JSON.parse(tasks);
-  };
-  
-  //  const setSelectedTask = (selectedTasksContext: any = null) => {
-  //   if (!selectedTasksContext) {
-  //     localStorage.deleteItem("selectedTask");
-  //   } else {
-  //     const selectedTaskValue = JSON.stringify(selectedTaskContext);
-  
-  //     localStorage.setItem("selectedTask", selectedTaskValue);
-  //   }
-  // };
-  
-   const getSelectedTask = () => {
-    let selectedTask = localStorage.getItem("selectedTask");
-    if (!selectedTask) {
-      return null;
-    }
-    return JSON.parse(selectedTask);
-  };
-  
+ localStorage.setItem("tasks", tasksValue);
+}
 
-  
-
-  return (
-    <TodoContext.Provider value={{ todos, setTasks  }}>
-      {children}
-    </TodoContext.Provider>
-  );
+const getTasks = () => {
+  let tasks = localStorage.getItem("tasks");
+  if (!tasks) {
+    return [];
+  }
+  return JSON.parse(tasks);
 };
 
-export default TodoProvider;
+export const setSelectedTask = (selectedTasksContext: any = null) => {
+  if (!selectedTasksContext) {
+    localStorage.deleteItem("selectedTask");
+  } else {
+    const selectedTaskValue = JSON.stringify(selectedTasksContext);
+
+    localStorage.setItem("selectedTask", selectedTaskValue);
+  }
+};
+
+export const getSelectedTask = () => {
+  let selectedTask = localStorage.getItem("selectedTask");
+  if (!selectedTask) {
+    return null;
+  }
+  return JSON.parse(selectedTask);
+};
+
+
+  
+
+
+
+export const ThemeContext = createContext<ThemeContextType>({setTasks,getTasks,getSelectedTask,setSelectedTask });
+export const useTheme = () => useContext(ThemeContext);
+
+
+
+
+  
+  
+   
+  
+  
+
+
+
 
 
 
