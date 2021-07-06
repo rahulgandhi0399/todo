2b7c81176708e4b51938a1e978a1b3c5e38eb394
Aomkar <aomkar.jagdale.rebase@gmail.com>
2021-07-06 14:15:50 +0530

    context created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, { useContext } from 'react';
-import TodoProvider from './context/tasks'
+import React from 'react';
+
 
 import './App.css';
-import  todos  from "./context/tasks";
+import  { ThemeContext, todo}  from "./context/tasks";
 import TaskForm from './TaskForm';
 
 
@@ -10,28 +10,26 @@ import TaskForm from './TaskForm';
 
 
 function App() {
- 
+  const [todos, setTodos] = React.useState(todo);
+  
+
   // const selectedTaskValue  = useContext(selectedTaskContext);
   // const tasksValue =   useContext(tasksContext);
   // tasksValue.push("dummy task")
   // console.log(selectedTaskValue,tasksValue);
   
   return (
-    <TodoProvider>
+    <ThemeContext.Provider  value={{todos,setTasks:setTodos}} >
     <div className="App">
       <header className="App-header">
        
         <p>
-          
-          
-          
           <TaskForm/>
-          
         </p>
         
       </header>
     </div>
-    </TodoProvider>
+    </ThemeContext.Provider>
   );
 }
 
